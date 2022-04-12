6c34f8e569c2cb9dd28f500025a73cc9b32ab2de
geloro94 <geloro94@gmail.com>
2022-04-12 13:42:53 +0200

    First working version with GET ui_profiles Endpoint and database migration for ui_profiles

--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/TerminologyService.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/TerminologyService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.numcodex.feasibility_gui_backend.terminology.api.CategoryEntry;
 import de.numcodex.feasibility_gui_backend.terminology.api.TerminologyEntry;
 import de.numcodex.feasibility_gui_backend.terminology.db.UiProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,12 @@ import java.util.stream.Stream;
 @Service
 public class TerminologyService {
 
-  private  String uiProfilePath;
-  private static final List<String> SORTED_CATEGORIES = List.of("Einwilligung", "Biobank", "Diagnose", "Fall", "Laborbefund", "Medikation", "Person", "Prozedur", "GECCO");
+  @Autowired
+  private UiProfileRepository uiProfileRepository;
+
+  private String uiProfilePath;
+  private static final List<String> SORTED_CATEGORIES = List.of("Einwilligung", "Biobank",
+      "Diagnose", "Fall", "Laborbefund", "Medikation", "Person", "Prozedur", "GECCO");
   private Map<UUID, TerminologyEntry> terminologyEntries = new HashMap<>();
   private List<CategoryEntry> categoryEntries = new ArrayList<>();
   private Map<UUID, TerminologyEntry> terminologyEntriesWithOnlyDirectChildren = new HashMap<>();
@@ -62,14 +67,13 @@ public class TerminologyService {
     System.out.println(uiProfilePath);
 
     return Stream.of(
-        Objects.requireNonNull(new File(uiProfilePath).listFiles()))
+            Objects.requireNonNull(new File(uiProfilePath).listFiles()))
         .filter(file -> !file.isDirectory())
         .map(File::getName)
         .collect(Collectors.toSet());
   }
 
   private void generateTerminologyEntriesWithOnlyDirectChildren(TerminologyEntry terminologyTree) {
-
 
     var entryWithOnlyDirectChildren = TerminologyEntry.copyWithDirectChildren(terminologyTree);
     terminologyEntriesWithOnlyDirectChildren
@@ -104,7 +108,8 @@ public class TerminologyService {
   }
 
   public List<CategoryEntry> getCategories() {
-    categoryEntries.sort(Comparator.comparing(value -> SORTED_CATEGORIES.indexOf(value.getDisplay())));
+    categoryEntries.sort(
+        Comparator.comparing(value -> SORTED_CATEGORIES.indexOf(value.getDisplay())));
     return categoryEntries;
   }
 
@@ -131,7 +136,9 @@ public class TerminologyService {
     if (categoryId != null) {
       return selectableEntriesByCategory.get(categoryId).stream()
           .sorted(Comparator
-              .comparingDouble((TerminologyEntry val) -> diceCoefficientOptimized(val.getDisplay(), query)).reversed()).limit(20)
+              .comparingDouble(
+                  (TerminologyEntry val) -> diceCoefficientOptimized(val.getDisplay(), query))
+              .reversed()).limit(20)
           .collect(Collectors.toList());
     } else {
       Set<TerminologyEntry> allSelectableEntries = new HashSet<>();
@@ -141,18 +148,23 @@ public class TerminologyService {
       System.out.println(allSelectableEntries.size());
       return allSelectableEntries.stream()
           .sorted(Comparator
-              .comparingDouble((TerminologyEntry val) -> diceCoefficientOptimized(val.getDisplay(), query)).reversed()).limit(20)
+              .comparingDouble(
+                  (TerminologyEntry val) -> diceCoefficientOptimized(val.getDisplay(), query))
+              .reversed()).limit(20)
           .collect(Collectors.toList());
     }
   }
 
-  public static TerminologyEntry getUiProfile(String system, String code, String version) {
-
+  public String getUiProfile(String system, String code, String version) {
+    if (Objects.nonNull(version)) {
+      return uiProfileRepository.findUiProfileByCoding(system, code, version).getUiProfile();
+    } else {
+      return uiProfileRepository.findUiProfileByCoding(system, code).getUiProfile();
+    }
 
   }
 
-  public static double diceCoefficientOptimized(String s, String t)
-  {
+  public static double diceCoefficientOptimized(String s, String t) {
     s = s.toLowerCase();
     t = t.toLowerCase();
     // Verifying the input:
@@ -167,7 +179,7 @@ public class TerminologyService {
 
     double result = 0;
     var subStrings = s.split(" ");
-    if(subStrings.length > 1) {
+    if (subStrings.length > 1) {
       for (var subString : s.split(" ")) {
         var coefficient = diceCoefficientOptimized(subString, t);
         if (coefficient > result) {
@@ -176,28 +188,31 @@ public class TerminologyService {
       }
     }
 
-
     // Create the bigrams for string s:
-    final int n = s.length()-1;
+    final int n = s.length() - 1;
     final int[] sPairs = new int[n];
-    for (int i = 0; i <= n; i++)
-      if (i == 0)
+    for (int i = 0; i <= n; i++) {
+      if (i == 0) {
         sPairs[i] = s.charAt(i) << 16;
-      else if (i == n)
-        sPairs[i-1] |= s.charAt(i);
-      else
-        sPairs[i] = (sPairs[i-1] |= s.charAt(i)) << 16;
+      } else if (i == n) {
+        sPairs[i - 1] |= s.charAt(i);
+      } else {
+        sPairs[i] = (sPairs[i - 1] |= s.charAt(i)) << 16;
+      }
+    }
 
     // Create the bigrams for string t:
-    final int m = t.length()-1;
+    final int m = t.length() - 1;
     final int[] tPairs = new int[m];
-    for (int i = 0; i <= m; i++)
-      if (i == 0)
+    for (int i = 0; i <= m; i++) {
+      if (i == 0) {
         tPairs[i] = t.charAt(i) << 16;
-      else if (i == m)
-        tPairs[i-1] |= t.charAt(i);
-      else
-        tPairs[i] = (tPairs[i-1] |= t.charAt(i)) << 16;
+      } else if (i == m) {
+        tPairs[i - 1] |= t.charAt(i);
+      } else {
+        tPairs[i] = (tPairs[i - 1] |= t.charAt(i)) << 16;
+      }
+    }
 
     // Sort the bigram lists:
     Arrays.sort(sPairs);
@@ -205,18 +220,16 @@ public class TerminologyService {
 
     // Count the matches:
     int matches = 0, i = 0, j = 0;
-    while (i < n && j < m)
-    {
-      if (sPairs[i] == tPairs[j])
-      {
+    while (i < n && j < m) {
+      if (sPairs[i] == tPairs[j]) {
         matches += 2;
         i++;
         j++;
-      }
-      else if (sPairs[i] < tPairs[j])
+      } else if (sPairs[i] < tPairs[j]) {
         i++;
-      else
+      } else {
         j++;
+      }
     }
 
     return Math.max(result, (double) matches / (n + m));
@@ -232,11 +245,9 @@ public class TerminologyService {
       for (int j = 0; j <= y.length(); j++) {
         if (i == 0) {
           dp[i][j] = j;
-        }
-        else if (j == 0) {
+        } else if (j == 0) {
           dp[i][j] = i;
-        }
-        else {
+        } else {
           dp[i][j] = min(dp[i - 1][j - 1]
                   + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
               dp[i - 1][j] + 1,
@@ -247,7 +258,6 @@ public class TerminologyService {
 
     return dp[x.length()][y.length()];
   }
-
 
 
   public static int costOfSubstitution(char a, char b) {
@@ -263,7 +273,7 @@ public class TerminologyService {
   private boolean matchesQuery(String query, TerminologyEntry terminologyEntry) {
     return terminologyEntry.getDisplay().toLowerCase().startsWith(query.toLowerCase()) ||
         Arrays.stream(terminologyEntry.getDisplay().toLowerCase().split(" "))
-        .anyMatch(var-> var.startsWith(query.toLowerCase())) ||
+            .anyMatch(var -> var.startsWith(query.toLowerCase())) ||
         (terminologyEntry.getTermCode() != null && terminologyEntry.getTermCode().getCode()
             .startsWith(query));
   }
--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/db/UiProfileContent.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/db/UiProfileContent.java
@@ -1,15 +1,18 @@
 package de.numcodex.feasibility_gui_backend.terminology.db;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.Table;
 import lombok.Data;
 
-@Entity
+@Entity(name = "UI_PROFILE")
+@Table(name = "UI_PROFILE_TABLE")
 @Data
 @IdClass(Coding.class)
-public class UiProfileContent {
+public class UiProfileContent implements Serializable {
   @Id
   @Column(name = "system")
   private String system;
@@ -22,6 +25,6 @@ public class UiProfileContent {
   @Column(name = "version")
   private String version;
 
-  @Column(name = "query_content", nullable = false)
-  private String queryContent;
+  @Column(name = "UI_Profile", nullable = false)
+  private String uiProfile;
 }
