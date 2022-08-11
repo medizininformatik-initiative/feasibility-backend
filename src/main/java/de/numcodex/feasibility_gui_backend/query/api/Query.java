e2c24e862c8c0939d28702bb88da3542ada7e4ca
Michael Folz <folz@med.uni-frankfurt.de>
2022-08-11 16:51:58 +0200

    #19 - Change stored query handling
    - include missing comment property in Query
    - correctly read the label of a query from the db

--- src/main/java/de/numcodex/feasibility_gui_backend/query/api/Query.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/query/api/Query.java
@@ -1,11 +1,14 @@
 package de.numcodex.feasibility_gui_backend.query.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.numcodex.feasibility_gui_backend.common.api.TermCode;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class Query {
 
   @JsonProperty
@@ -14,6 +17,8 @@ public class Query {
   private StructuredQuery content;
   @JsonProperty
   private String label;
+  @JsonProperty
+  private String comment;
   @JsonProperty
   private QueryResult results;
   @JsonProperty
--- src/main/java/de/numcodex/feasibility_gui_backend/query/persistence/SavedQueryRepository.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/query/persistence/SavedQueryRepository.java
@@ -1,7 +1,11 @@
 package de.numcodex.feasibility_gui_backend.query.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SavedQueryRepository extends JpaRepository<SavedQuery, Long> {
 
+  @Query("SELECT sq FROM SavedQuery sq left join Query q ON sq.query.id = q.id WHERE q.id = ?1")
+  Optional<SavedQuery> findByQueryId(Long queryId);
 }
