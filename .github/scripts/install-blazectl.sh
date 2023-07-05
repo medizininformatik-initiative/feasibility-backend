6a03bcc7ef1c95de150fa901f64f0a84da66e6b6
Michael Folz <folz@med.uni-frankfurt.de>
2023-07-05 14:58:40 +0200

    #138 - Extend integration tests in ci
    - remove large amount of testdata

--- .github/scripts/install-blazectl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-VERSION="0.13.0"
-
-curl -sLO "https://github.com/samply/blazectl/releases/download/v$VERSION/blazectl-$VERSION-linux-amd64.tar.gz"
-tar xzf "blazectl-$VERSION-linux-amd64.tar.gz"
-rm "blazectl-$VERSION-linux-amd64.tar.gz"
-sudo mv ./blazectl /usr/local/bin/blazectl
