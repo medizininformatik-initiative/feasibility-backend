fd7562e5de713d522753aa8556173eb59e82af8c
Michael Folz <folz@med.uni-frankfurt.de>
2023-10-04 16:10:17 +0200

    #210 - Add Basic Auth to direct broker
    - add username and password env vars
    - add auth interceptors to both fhir (cql) and spring (flare) clients
    - (unrelated...add volume to docker compose file)

--- src/main/java/de/numcodex/feasibility_gui_backend/query/broker/direct/DirectSpringConfig.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/query/broker/direct/DirectSpringConfig.java
@@ -1,7 +1,9 @@
 package de.numcodex.feasibility_gui_backend.query.broker.direct;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IClientInterceptor;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
 import de.numcodex.feasibility_gui_backend.query.broker.BrokerClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 /**
  * Spring configuration for providing a {@link DirectBrokerClient} implementation instance.
@@ -18,36 +22,55 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class DirectSpringConfig {
 
-    @Value("${app.broker.direct.useCql:false}")
-    private boolean useCql;
+    private final boolean useCql;
 
-    @Value("${app.flare.baseUrl}")
-    private String flareBaseUrl;
+    private final String flareBaseUrl;
 
-    @Value("${app.cql.baseUrl}")
-    private String cqlBaseUrl;
+    private final String cqlBaseUrl;
+
+    private final String username;
+
+    private final String password;
+
+    public DirectSpringConfig(@Value("${app.broker.direct.useCql:false}") boolean useCql, @Value("${app.flare.baseUrl}") String flareBaseUrl, @Value("${app.cql.baseUrl}") String cqlBaseUrl, @Value("${app.broker.direct.auth.basic.username}") String username, @Value("${app.broker.direct.auth.basic.password}") String password) {
+        this.useCql = useCql;
+        this.flareBaseUrl = flareBaseUrl;
+        this.cqlBaseUrl = cqlBaseUrl;
+        this.username = username;
+        this.password = password;
+    }
 
     @Qualifier("direct")
     @Bean
     public BrokerClient directBrokerClient(WebClient directWebClientFlare, @Value("${app.broker.direct.obfuscateResultCount:false}") boolean obfuscateResultCount,
         FhirConnector fhirConnector, FhirHelper fhirHelper) {
         if (useCql) {
-
-            return new DirectBrokerClientCql(fhirConnector, obfuscateResultCount,
-                fhirHelper);
+            return new DirectBrokerClientCql(fhirConnector, obfuscateResultCount, fhirHelper);
         } else {
             return new DirectBrokerClientFlare(directWebClientFlare, obfuscateResultCount);
         }
     }
 
     @Bean
-    public IGenericClient getFhirClient(FhirContext fhirContext){
-        return fhirContext.newRestfulGenericClient(cqlBaseUrl);
+    public IGenericClient getFhirClient(FhirContext fhirContext) {
+        IGenericClient iGenericClient = fhirContext.newRestfulGenericClient(cqlBaseUrl);
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+            IClientInterceptor authInterceptor = new BasicAuthInterceptor(username, password);
+            iGenericClient.registerInterceptor(authInterceptor);
+        }
+        return iGenericClient;
     }
 
     @Bean
     public WebClient directWebClientFlare() {
-        return WebClient.create(flareBaseUrl);
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+            return WebClient.builder()
+                    .filter(basicAuthentication(username, password))
+                    .baseUrl(flareBaseUrl)
+                    .build();
+        } else {
+            return WebClient.create(flareBaseUrl);
+        }
     }
 
 }
