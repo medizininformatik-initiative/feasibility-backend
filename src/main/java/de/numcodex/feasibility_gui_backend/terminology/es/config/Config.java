83fe9f436a250ebac4cabc829a5f2f2c95647223
Michael Folz <folz@med.uni-frankfurt.de>
2024-05-17 13:31:43 +0200

    #287 - Elastic search connection
    - add a switch to turn elastic search on or off

--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/es/config/Config.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/es/config/Config.java
@@ -1,9 +1,11 @@
 package de.numcodex.feasibility_gui_backend.terminology.es.config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
+@ConditionalOnExpression("${app.elasticSearchEnabled}")
 public class Config extends ElasticsearchConfiguration {
 
   @Override
--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/es/repository/OntologyItemEsRepository.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/es/repository/OntologyItemEsRepository.java
@@ -1,6 +1,8 @@
 package de.numcodex.feasibility_gui_backend.terminology.es.repository;
 
 import de.numcodex.feasibility_gui_backend.terminology.es.model.OntologyItemDocument;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+@ConditionalOnExpression("${app.elasticSearchEnabled}")
 public interface OntologyItemEsRepository extends ElasticsearchRepository<OntologyItemDocument, String> {}
