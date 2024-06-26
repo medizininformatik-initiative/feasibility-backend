37d46441464ee2f02a41c5f8d010c1534f98f389
Mathias Rühle <math2306@hotmail.com>
2024-06-26 09:42:08 +0200

    Add OpenID Connect authentication in direct broker (#303)
    * Add Maven Schema Location
    
    * Add OAuth Authentication Option for Direct Broker
    
    * Update JaCoCo to Version 0.8.12 to Support Java 22
    
    * #302 - Add OpenID Connect authentication in direct broker
    
    - remove typo from readme
    - align indentation in DirectSpringConfigTest
    
    ---------
    
    Co-authored-by: Michael Folz <folz@med.uni-frankfurt.de>

---
+++ src/main/java/de/numcodex/feasibility_gui_backend/query/broker/OAuthInterceptor.java
@@ -0,0 +1,81 @@
+package de.numcodex.feasibility_gui_backend.query.broker;
+
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.client.api.IClientInterceptor;
+import ca.uhn.fhir.rest.client.api.IHttpRequest;
+import ca.uhn.fhir.rest.client.api.IHttpResponse;
+import com.nimbusds.oauth2.sdk.AccessTokenResponse;
+import com.nimbusds.oauth2.sdk.ClientCredentialsGrant;
+import com.nimbusds.oauth2.sdk.GeneralException;
+import com.nimbusds.oauth2.sdk.TokenErrorResponse;
+import com.nimbusds.oauth2.sdk.TokenRequest;
+import com.nimbusds.oauth2.sdk.TokenResponse;
+import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+import lombok.NonNull;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.net.URI;
+
+public final class OAuthInterceptor implements IClientInterceptor {
+
+    private static final int TOKEN_EXPIRY_THRESHOLD = 5000;
+    private HTTPRequest tokenRequest;
+    private AccessToken token;
+    private DateTime tokenExpiry;
+    private Issuer issuer;
+    private ClientSecretBasic clientAuth;
+
+    public OAuthInterceptor(@NonNull String oauthIssuerUrl, @NonNull String oauthClientId,
+            @NonNull String oauthClientSecret) {
+        clientAuth = new ClientSecretBasic(new ClientID(oauthClientId), new Secret(oauthClientSecret));
+        issuer = new Issuer(oauthIssuerUrl);
+    }
+
+    public String getToken() {
+        if (token == null || DateTime.now().plus(TOKEN_EXPIRY_THRESHOLD).isAfter(tokenExpiry)) {
+            try {
+                TokenResponse response = TokenResponse.parse(getTokenRequest().send());
+                if (!response.indicatesSuccess()) {
+                    TokenErrorResponse errorResponse = response.toErrorResponse();
+                    throw new OAuthClientException(errorResponse.getErrorObject().getCode() + " - "
+                            + errorResponse.getErrorObject().getDescription());
+                }
+                AccessTokenResponse successResponse = response.toSuccessResponse();
+
+                token = successResponse.getTokens().getAccessToken();
+                tokenExpiry = DateTime.now().plus(token.getLifetime() * 1000);
+            } catch (GeneralException | IOException e) {
+                throw new OAuthClientException("Request for OAuth2 access token failed", e);
+            }
+        }
+        return token.getValue();
+    }
+
+    private HTTPRequest getTokenRequest() throws GeneralException, IOException {
+        if (tokenRequest == null) {
+            tokenRequest = new TokenRequest(getTokenUri(), clientAuth, new ClientCredentialsGrant()).toHTTPRequest();
+        }
+        return tokenRequest;
+    }
+
+    private URI getTokenUri() throws GeneralException, IOException {
+        return OIDCProviderMetadata.resolve(issuer).getTokenEndpointURI();
+    }
+
+    @Override
+    public void interceptRequest(IHttpRequest theRequest) {
+        theRequest.addHeader(Constants.HEADER_AUTHORIZATION,
+                Constants.HEADER_AUTHORIZATION_VALPREFIX_BEARER + getToken());
+    }
+
+    @Override
+    public void interceptResponse(IHttpResponse theResponse) throws IOException {
+    }
+}
--- src/main/java/de/numcodex/feasibility_gui_backend/query/broker/direct/DirectSpringConfig.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/query/broker/direct/DirectSpringConfig.java
@@ -1,10 +1,11 @@
 package de.numcodex.feasibility_gui_backend.query.broker.direct;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.client.api.IClientInterceptor;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
 import de.numcodex.feasibility_gui_backend.query.broker.BrokerClient;
+import de.numcodex.feasibility_gui_backend.query.broker.OAuthInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,38 +13,49 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 /**
- * Spring configuration for providing a {@link DirectBrokerClient} implementation instance.
- * Either {@link DirectBrokerClientCql} or {@link DirectBrokerClientFlare}
+ * Spring configuration for providing a {@link DirectBrokerClient} implementation instance. Either
+ * {@link DirectBrokerClientCql} or {@link DirectBrokerClientFlare}
  */
 @Lazy
 @Configuration
+@Slf4j
 public class DirectSpringConfig {
 
     private final boolean useCql;
-
     private final String flareBaseUrl;
-
     private final String cqlBaseUrl;
-
     private final String username;
-
     private final String password;
+    private String issuer;
+    private String clientId;
+    private String clientSecret;
 
-    public DirectSpringConfig(@Value("${app.broker.direct.useCql:false}") boolean useCql, @Value("${app.flare.baseUrl}") String flareBaseUrl, @Value("${app.cql.baseUrl}") String cqlBaseUrl, @Value("${app.broker.direct.auth.basic.username}") String username, @Value("${app.broker.direct.auth.basic.password}") String password) {
+    public DirectSpringConfig(@Value("${app.broker.direct.useCql:false}") boolean useCql,
+            @Value("${app.flare.baseUrl}") String flareBaseUrl, @Value("${app.cql.baseUrl}") String cqlBaseUrl,
+            @Value("${app.broker.direct.auth.basic.username}") String username,
+            @Value("${app.broker.direct.auth.basic.password}") String password,
+            @Value("${app.broker.direct.auth.oauth.issuer.url}") String issuer,
+            @Value("${app.broker.direct.auth.oauth.client.id") String clientId,
+            @Value("${app.broker.direct.auth.oauth.client.secret}") String clientSecret) {
         this.useCql = useCql;
         this.flareBaseUrl = flareBaseUrl;
         this.cqlBaseUrl = cqlBaseUrl;
         this.username = username;
         this.password = password;
+        this.issuer = issuer;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
     }
 
     @Qualifier("direct")
     @Bean
-    public BrokerClient directBrokerClient(WebClient directWebClientFlare, @Value("${app.broker.direct.obfuscateResultCount:false}") boolean obfuscateResultCount,
-        FhirConnector fhirConnector, FhirHelper fhirHelper) {
+    public BrokerClient directBrokerClient(WebClient directWebClientFlare,
+                                           @Value("${app.broker.direct.obfuscateResultCount:false}") boolean obfuscateResultCount,
+                                           FhirConnector fhirConnector, FhirHelper fhirHelper) {
         if (useCql) {
             return new DirectBrokerClientCql(fhirConnector, obfuscateResultCount, fhirHelper);
         } else {
@@ -54,21 +66,31 @@ public class DirectSpringConfig {
     @Bean
     public IGenericClient getFhirClient(FhirContext fhirContext) {
         IGenericClient iGenericClient = fhirContext.newRestfulGenericClient(cqlBaseUrl);
-        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
-            IClientInterceptor authInterceptor = new BasicAuthInterceptor(username, password);
-            iGenericClient.registerInterceptor(authInterceptor);
+        if (!isNullOrEmpty(password) && !isNullOrEmpty(username)) {
+            log.info("Enable direct broker with basic authentication (type: cql, url: {}, username: {})", cqlBaseUrl,
+                    username);
+            iGenericClient.registerInterceptor(new BasicAuthInterceptor(username, password));
+        } else if (!isNullOrEmpty(issuer) && !isNullOrEmpty(clientId) && !isNullOrEmpty(clientSecret)) {
+            log.info("Enable direct broker with oauth authentication (type: cql, url: {}, issuer: {}, client-id: {})",
+                    cqlBaseUrl, issuer, clientId);
+            iGenericClient.registerInterceptor(new OAuthInterceptor(issuer, clientId, clientSecret));
+        } else {
+            log.info("Enable direct broker (type: cql, url: {})", cqlBaseUrl);
         }
         return iGenericClient;
     }
 
     @Bean
     public WebClient directWebClientFlare() {
-        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+        if (!isNullOrEmpty(password) && !isNullOrEmpty(username)) {
+            log.info("Enable direct broker with basic authentication (type: flare, url: {}, username: {})",
+                    flareBaseUrl, username);
             return WebClient.builder()
                     .filter(basicAuthentication(username, password))
                     .baseUrl(flareBaseUrl)
                     .build();
         } else {
+            log.info("Enable direct broker (type: flare, url: {})", flareBaseUrl);
             return WebClient.create(flareBaseUrl);
         }
     }
