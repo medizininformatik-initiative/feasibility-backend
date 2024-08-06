0ddbd8e0338214a94c1c0ba25a57ee91524c32ea
Michael Folz <folz@med.uni-frankfurt.de>
2024-08-06 13:53:18 +0200

    #319 - Fix github action error
    - change addgroup/adduser to groupadd/useradd cmds

--- .github/scripts/create-keycloak-user.sh
+++ .github/scripts/create-keycloak-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker exec -u0 integration-test_auth_1 /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keycloakadmin --password keycloak
-docker exec -u0 integration-test_auth_1 /opt/keycloak/bin/kcadm.sh create users -s username=testuser -s email=test@example.com -s enabled=true -s emailVerified=true -r feasibility
-docker exec -u0 integration-test_auth_1 /opt/keycloak/bin/kcadm.sh add-roles --uusername testuser --rolename FeasibilityUser -r feasibility
-docker exec -u0 integration-test_auth_1 /opt/keycloak/bin/kcadm.sh set-password -r feasibility --username testuser --new-password testpassword
+docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keycloakadmin --password keycloak
+docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh create users -s username=testuser -s email=test@example.com -s enabled=true -s emailVerified=true -r feasibility
+docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh add-roles --uusername testuser --rolename FeasibilityUser -r feasibility
+docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh set-password -r feasibility --username testuser --new-password testpassword
