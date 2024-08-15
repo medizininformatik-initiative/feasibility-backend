46595a0de84bf7d3a25810bb5c4d1fac7dd203ae
Michael Folz <folz@med.uni-frankfurt.de>
2024-08-15 14:47:46 +0200

    #318 - Add endpoints for profile data
    - in dse profile filter, change single referencedCriteriaSet to list of value set urls
    - change v9_1 script to v9 since there is no 2nd part any more (is a repeatable now)

--- src/main/java/de/numcodex/feasibility_gui_backend/dse/api/Filter.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/dse/api/Filter.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record Filter(
     @JsonProperty String type,
     @JsonProperty String name,
     @JsonProperty(value = "ui_type") String uiType,
-    @JsonProperty String referencedCriteriaSet
+    @JsonProperty List<String> valueSetUrls
 ) {
 }
