4ee7673e7d78b1793492f03eddcbe17d56b0637a
Michael Folz <folz@med.uni-frankfurt.de>
2024-08-23 08:41:52 +0200

    #289 - Initialise elastic search as part of a standard deployment
    - make the git tag configurable via env variable
    - add health check to elastic search container
    - change depends_on conditions
    - exit init container with error when file could not be downloaded
    - remove sed cmd to replace misspelled variable name

--- elastic-init.sh
+++ elastic-init.sh
@@ -6,10 +6,16 @@ until curl -X GET "$ELASTIC_HOST/_cluster/health" | grep -q '"status":"green"\|"
     sleep 5
 done
 
-echo "Downloading $ELASTIC_FILEPATH$ELASTIC_FILENAME"
-curl -sLO "$ELASTIC_FILEPATH$ELASTIC_FILENAME"
+ABSOLUTE_FILEPATH="${ELASTIC_FILEPATH//TAGPLACEHOLDER/$ELASTIC_GIT_TAG}$ELASTIC_FILENAME"
+echo "Downloading $ABSOLUTE_FILEPATH"
+response_onto_dl=$(curl --write-out "%{http_code}" -sLO "$ABSOLUTE_FILEPATH")
 
-unzip -o $ELASTIC_FILENAME
+if [ "$response_onto_dl" -ne 200 ]; then
+  echo "Could not download ontology file. Maybe the tag $ELASTIC_GIT_TAG does not exist? Error code was $response_onto_dl"
+  exit 1
+fi
+
+unzip -o "$ELASTIC_FILENAME"
 
 if [ "$OVERRIDE_EXISTING" = "true" ]; then
   echo "(Trying to) delete existing indices"
@@ -37,7 +43,6 @@ for FILE in elastic/*; do
     if [[ $BASENAME == onto_es__codeable_concept* && $BASENAME == *.json ]]; then
       if [[ "$response_cc" -eq 200 || "$OVERRIDE_EXISTING" = "true" ]]; then
         echo "Uploading $BASENAME"
-        sed -i 's/valuesets/value_sets/g' "$FILE"
         curl -s --output /dev/null -XPOST -H 'Content-Type: application/json' --data-binary @"$FILE" "$ELASTIC_HOST/codeable_concept/_bulk?pretty"
       else
         echo "Skipping $BASENAME because index was already existing. Set OVERRIDE_EXISTING to true to force creating a new index"
