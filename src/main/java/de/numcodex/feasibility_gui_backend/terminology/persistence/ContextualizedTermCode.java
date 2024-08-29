017676a617d5e707e2bc3effd620d00c6d7c25c9
Michael Folz <folz@med.uni-frankfurt.de>
2024-08-29 12:40:08 +0200

    #332 - Remove @Data annotation from jpa classes
    - Remove @Data and @EqualsAndHashCode annotations and replace them with @Getter @Setter @ToString (excluding lazily loaded variables) and custom equals and hashcode methods
    - Change id datatype from int to Long in entity classes

--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/persistence/ContextualizedTermCode.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/persistence/ContextualizedTermCode.java
@@ -1,13 +1,17 @@
 package de.numcodex.feasibility_gui_backend.terminology.persistence;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "contextualized_termcode", schema = "public")
-@Data
-@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class ContextualizedTermCode {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -25,4 +29,20 @@ public class ContextualizedTermCode {
     @Basic
     @Column(name = "ui_profile_id", nullable = true)
     private Integer uiProfileId;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ContextualizedTermCode that = (ContextualizedTermCode) o;
+        return getContextTermcodeHash() != null && Objects.equals(getContextTermcodeHash(), that.getContextTermcodeHash());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/de/numcodex/feasibility_gui_backend/terminology/persistence/UiProfile.java
+++ src/main/java/de/numcodex/feasibility_gui_backend/terminology/persistence/UiProfile.java
@@ -1,22 +1,42 @@
 package de.numcodex.feasibility_gui_backend.terminology.persistence;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "ui_profile", schema = "public")
-@Data
-@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class UiProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id", nullable = false)
-    private int id;
+    private Long id;
     @Basic
     @Column(name = "name", nullable = false, length = -1)
     private String name;
     @Basic
     @Column(name = "ui_profile", columnDefinition = "json", nullable = false)
     private String uiProfile;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        UiProfile uiProfile = (UiProfile) o;
+        return getId() != null && Objects.equals(getId(), uiProfile.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
